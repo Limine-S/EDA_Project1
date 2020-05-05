fe67893e04bcf760badc5ac46b81492ca06adad4
Limine-S <55068044+Limine-S@users.noreply.github.com>
2020-05-05 12:42:34 +0800

    Add files via upload

---
+++ Plot4.R
@@ -0,0 +1,33 @@
+# read data into R
+files <- file('./Data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), 
+                  sep = ';', col.names = c("Date", "Time", "Global_active_power", 
+                                           "Global_reactive_power", "Voltage", 
+                                           "Global_intensity", "Sub_metering_1", 
+                                           "Sub_metering_2", "Sub_metering_3"), 
+                  na.strings = '?')
+
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+# open device
+png(filename = './Figures/plot4.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "en_US")
+par(mfrow = c(2, 2))
+plot(data$DateTime, data$Global_active_power, xlab = '', 
+     ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', 
+     type = 'l')
+plot(data$DateTime, data$Sub_metering_1, xlab = '', 
+     ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), 
+       legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), 
+       lwd = 1)
+plot(data$DateTime, data$Global_active_power, xlab = '', 
+     ylab = 'Global Active Power (kilowatt)', type = 'l')
+# close device
+dev.off()
